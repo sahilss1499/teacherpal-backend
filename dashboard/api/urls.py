fce9450517733dfcb2973bbf4f253713bda66b89
Sahil Sinha <sahilss1499@gmail.com>
2021-06-19 15:13:54 +0530

    minor changes

--- dashboard/api/urls.py
+++ dashboard/api/urls.py
@@ -6,9 +6,9 @@ from .batches_views import (BatchCreateListView, BatchDetailView, BatchStudentLi
 
 
 urlpatterns = [
-    path('signup/', SignUp.as_view()),
-    path('login/', LoginAPIView.as_view()),
-    path('save-fcm-token/', SaveFCMToken.as_view()),
+    path('signup', SignUp.as_view()),
+    path('login', LoginAPIView.as_view()),
+    path('save-fcm-token', SaveFCMToken.as_view()),
 
     path('batch', BatchCreateListView.as_view()),
     path('batch/<int:pk>', BatchDetailView.as_view()),
